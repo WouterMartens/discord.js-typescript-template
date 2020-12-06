6cc72d283cf0ca2ef06c8d0011f9b2dd64050a96
Wouter <woutermartens94@gmail.com>
2020-12-06 19:20:53 +0100

    Add command handler, cooldown, client.commands

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,14 @@
 import { Message } from "discord.js";
 
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	execute(message: Message) {
+    name: 'ping',
+    description: 'Ping, pong',
+    args: false,
+    aliases: [],
+    usage: 'ping',
+    guildOnly: true,
+    cooldown: 3,
+	execute(message: Message, args: string[]) {
 		message.channel.send('Pong.');
 	},
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { Client, Collection } from 'discord.js';
 import fs from 'fs'; // File system
-import Client from "./MyClient.js"; // Client adapter to add commands functionality
-const { prefix, token } = require('../config.json'); // Config file
+const { prefix, token, cooldown } = require('../config.json'); // Config file
 
 const client: Client = new Client(); // Initialize client
 // Uses the file system to read the commands folder and filter all the .js files
@@ -14,22 +14,72 @@ for (const file of commandFiles) {
 
 client.login(token); // Login to the client
 
+const cooldowns = new Collection();
+
 // Wait for the client to be ready
 client.once('ready', () => {
     console.log('Ready!');
 });
 
+// When the client receives a new message
 client.on('message', message => {
+    // Checks if the message has the prefix and isn't made by a bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args: string[] = message.content.slice(prefix.length).trim().split('/ +/');
-    const command: string|undefined = args.shift()?.toLocaleLowerCase();
+    // Pulls the command and the arguments from the message
+    const args: string[] = message.content.slice(prefix.length).trim().split(/ +/);
+    //@ts-ignore
+    const commandName: string = args.shift()?.toLocaleLowerCase();
 
-    if (command && !client.commands.has(command)) return;
+    console.log(commandName, args);
+
+    // Checks if the command exists
+    const command = client.commands.get(commandName) ||
+        client.commands.find(cmd =>
+            cmd.aliases &&
+            cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    // Checks if the command is allowed to be used in a direct message
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply("I'm not allowed to execute that command inside a direct message");
+    }
+
+    // Checks if the command has arguments
+    if (command.args && !args.length) {
+        let reply = `${message.author}, you didn't provide any arguments`;
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Collection());
+    }
+
+    const now: number = Date.now();
+    const timestamps: any = cooldowns.get(command.name);
+    const cooldownAmount: number = (command.cooldown || cooldown) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
-        if (command) {
-            client.commands.get(command).execute(message, args);
+        // Execute the command
+        if (commandName) {
+            command.execute(message, args);
         }
     } catch (error) {
         console.error(error);
